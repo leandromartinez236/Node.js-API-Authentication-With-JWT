3d46c1e5d0e0a0fbd950f75784b8f8641ad8afc2
leandro <martinezleandro2004@gmail.com>
2022-09-01 14:54:59 -0300

    connect to db

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 
+dotenv.config()
+
+//connect to DB
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => console.log('Connect to DB'))
 //import routes
 const authRoute = require('./routes/auth')
 
