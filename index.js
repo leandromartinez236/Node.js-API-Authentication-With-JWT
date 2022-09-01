4bf437c55ac90cb507d0c7641bc0f79f42bfc172
leandro <martinezleandro2004@gmail.com>
2022-09-01 17:15:00 -0300

    modelo de usuario y ruta para registro creados

--- index.js
+++ index.js
@@ -3,14 +3,19 @@ const app = express()
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 
-dotenv.config()
-
-//connect to DB
-mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => console.log('Connect to DB'))
 //import routes
 const authRoute = require('./routes/auth')
 
+dotenv.config()
+//connect to DB
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true })
+  .then(() => console.log('Mongodb is connected'))
+  .catch(e => console.log(e))
+
 //middlewares
+app.use(express.json())
+
+//middlewares routes
 app.use('/api/user', authRoute)
 
 app.listen(3000, () => console.log('Server is running on port', 3000))
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,19 @@
 const router = require('express').Router()
+const User = require('../model/User')
 
 router.post('/register', async (req, res) => {
-  res.send('Register')
+  try {
+    const { name, email, password } = req.body
+    const newUser = new User({
+      name,
+      email,
+      password
+    })
+    const savedUser = await newUser.save()
+    res.send(savedUser)
+  } catch (error) {
+    res.status(400).json(error)
+  }
 })
 
 module.exports = router
